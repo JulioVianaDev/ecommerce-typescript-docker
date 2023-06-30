a643d1fc0be3096c86cef145f9bc85f3ade96995
juliovianadev <juliovianadev@gmail.com>
2023-06-30 00:04:07 -0300

    network falta funcionar

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -6,6 +6,7 @@ function App() {
   useEffect(()=>{
     axios.get('http://localhost:3001')
       .then(res=>console.log(res.data))
+      .catch(err=>console.log(err.message))
   },[])
   return (
     <div className="App">
