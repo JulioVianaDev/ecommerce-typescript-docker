acac25cf8cf69261ef1b4cb67f9eff2156234d3e
juliovianadev <juliovianadev@gmail.com>
2023-07-13 14:43:29 -0300

    mongodb funcionando

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,7 +1,17 @@
 import express from 'express';
 import {json }from 'body-parser';
+
 import InitialRoute from './routes/initial'
 const cors = require('cors');
+const mongoose = require('mongoose')
+
+mongoose
+  .connect(
+    'mongodb://mongodb:27017/docker-node-mongo',
+    { useNewUrlParser: true }
+  )
+  .then(() => console.log('MongoDB Conectado'))
+  .catch((err: any) => console.log(err));
 
 const app = express();
 require('dotenv').config()
@@ -12,7 +22,7 @@ app.use(cors())
 const a = process.env.TESTE
 
 app.get('/',(req,res)=>{
-  res.send({message: "funcionou de novo",a: a})
+  res.send({message: "funcionou de a",a: a})
 })
 app.listen(3001,()=>{
    console.log('rodando na porta 3001')
